952c33436c92e08680d1eb959485387f5b9c0c11
bliuag <bliuag@connect.ust.hk>
2016-10-01 13:56:55 +0800

    configuration added

--- worker.js
+++ worker.js
@@ -1,12 +1,14 @@
+var config=require("./config");
+var RedisSMQ = require("rsmq");
+var request = require('request');
 var worker=function(){
-
-	RedisSMQ = require("rsmq");
-	rsmq = new RedisSMQ( {host: "127.0.0.1", port: 6379, ns: "rsmq"} );
-
-	rsmq.receiveMessage({qname:"trade"}, function (err, resp) {
+    
+	rsmq = new RedisSMQ( {host: config.rmsq.IP, port: config.rmsq.port, ns: config.rmsq.ns} );
+	rsmq.receiveMessage({qname:config.rmsq.q1name}, function (err, resp) {
 	    if (resp.id) {
 	        console.log("Message received.", resp);
-	        rsmq.deleteMessage({qname:"trade", id:resp.id}, function (err, resp) {
+            var msg=resp.message;
+	        rsmq.deleteMessage({qname:config.rmsq.q1name, id:resp.id}, function (err, resp) {
 			    if (resp===1) {
 			        console.log("Message deleted.") 
 			    }
@@ -14,15 +16,14 @@ var worker=function(){
 			        console.log("Message not found.")
 			    }
 			});
-			var obj=JSON.parse(resp.message);
+			var obj=JSON.parse(msg);
 			var url="";
 			url=url.concat("http://cis2016-exchange",obj.exchange,".herokuapp.com/api/orders");
-			var request = require('request');
 			var options = {
 			  uri: url,
 			  method: 'POST',
 			  json: {
-			  	"team_uid":teamUID,
+			  	"team_uid":config.token,
 			  	"symbol":obj.symbol,
 			  	"side":obj.side,
 			  	"qty":obj.qty,
@@ -32,7 +33,7 @@ var worker=function(){
 
 			request(options, function (error, response, body) {
 			  if (!error && response.statusCode == 200) {
-			    rsmq.sendMessage({qname:"db", message:body}, function (err, resp) {
+			    rsmq.sendMessage({qname:config.rmsq.q2name, message:body}, function (err, resp) {
 				    if (resp) {
 				        console.log("Message sent. ID:", resp);
 				    }
