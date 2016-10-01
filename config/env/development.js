8762bf3992d6de70eda30570f9bddc0073d6b379
Ace Bai <cbai@connect.ust.hk>
2016-10-01 13:42:28 +0800

    add db info in config

--- config/env/development.js
+++ config/env/development.js
@@ -3,27 +3,35 @@
  * Expose
  */
 
-module.exports = {
-  db: 'mongodb://localhost/your_project_development',
-  token: "ub3xoKYYnv5007VCzJV_HA",
-  facebook: {
-    clientID: 'APP_ID',
-    clientSecret: 'SECRET',
-    callbackURL: 'http://localhost:3000/auth/facebook/callback',
-    scope: [
-      'email',
-      'user_about_me',
-      'user_friends'
-    ]
-  },
-  google: {
-    clientID: 'APP_ID',
-    clientSecret: 'SECRET',
-    callbackURL: 'http://localhost:3000/auth/google/callback',
-    scope: [
-      'https://www.googleapis.com/auth/userinfo.profile',
-      'https://www.googleapis.com/auth/userinfo.email',
-      'https://www.google.com/m8/feeds',
-    ]
-  }
-};
+ module.exports = {
+   db: 'mongodb://localhost/teamAce',
+   token: "ub3xoKYYnv5007VCzJV_HA",
+   mongo: {
+     uri: "mongodb://localhost:27017/teamAce"
+   },
+   rsmq: {
+     IP: "127.0.0.1",
+     port: 6379,
+     ns: "rsmq"
+   },
+   facebook: {
+     clientID: 'APP_ID',
+     clientSecret: 'SECRET',
+     callbackURL: 'http://localhost:3000/auth/facebook/callback',
+     scope: [
+       'email',
+       'user_about_me',
+       'user_friends'
+     ]
+   },
+   google: {
+     clientID: 'APP_ID',
+     clientSecret: 'SECRET',
+     callbackURL: 'http://localhost:3000/auth/google/callback',
+     scope: [
+       'https://www.googleapis.com/auth/userinfo.profile',
+       'https://www.googleapis.com/auth/userinfo.email',
+       'https://www.google.com/m8/feeds',
+     ]
+   }
+ };
