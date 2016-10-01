c57f4b0781a0001a4ab45c2c9de0699ef0e42b78
bliuag <bliuag@connect.ust.hk>
2016-10-02 04:06:59 +0800

    chart part1

--- public/js/main.js
+++ public/js/main.js
@@ -43,3 +43,16 @@ app.controller("tradingDisplayController", ["$scope", "$firebaseArray",
 
     }
 ]);
+
+angular.module("app", ["chart.js"]).controller("BarCtrl", function ($scope) {
+  $scope.labels = ['0005', '0386', '0388', '3988'];
+  $scope.series = ['Holding', 'Reserved'];
+
+  $scope.data = [
+    [65, 59, 80, 81],
+    [28, 48, 40, 19]
+  ];
+
+  
+});
+
