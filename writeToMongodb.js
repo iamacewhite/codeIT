ae1fc35a763c50354f43197781f0d15d10d74180
He Zhou <hezhou556@gmail.com>
2016-10-01 15:06:21 +0800

    Change the RSMQ create,send,receive,delete to 4 callbacks

--- writeToMongodb.js
+++ writeToMongodb.js
@@ -16,10 +16,10 @@ app.use(bodyParser.json());
 
 //Include redis modules
 RedisSMQ = require("rsmq");
-rsmq = new RedisSMQ( {host: config.rsmq.IP, port: config.rsmq.port, ns: config.rsmq.ns} );
+rsmq = new RedisSMQ({host: config.rsmq.IP, port: config.rsmq.port, ns: config.rsmq.ns});
 
 
-function insertToDB(entry){
+function insertToDB(entry) {
     mongoClient.connect(config.mongo.uri, function (err, db) {
         if (err) {
             console.log('an error');
@@ -28,7 +28,7 @@ function insertToDB(entry){
             db.createCollection('executionHistory', function (err, collection) {
                 db.collection('executionHistory').insertOne(entry, function (err, result) {
                 });
-                collection.find({}).toArray(function(err, result){
+                collection.find({}).toArray(function (err, result) {
                     console.log(result);
                 });
             });
@@ -36,51 +36,52 @@ function insertToDB(entry){
     });
 }
 
+/*
+function queryFromMongodb(criteria) {
+    mongoClient.connect(config.mongo.uri, function (err, db) {
+        if(!err){
 
-rsmq.createQueue({qname:config.rsmq.q2name}, function (err, resp) {
-    if(err){
-        console.log(err);
-    }
-    console.log('complete');
-    if (resp===1) {
-        console.log("queue created")
-    }
-});
-
-rsmq.sendMessage({qname: config.rsmq.q2name, message:JSON.stringify({
-    "symbol": "0001",
-    "side": "sell",
-    "qty": 1,
-    "order_type": "limit",
-    "price": 200,
-    "team_uid": "eETKmZ1JvPC2wM_UsA8sHw",
-    "exchange_id": 1
-})}, function (err, resp) {
-    if (resp) {
-        console.log("Message sent. ID:", resp);
-    }
-});
+        }
+    });
+}
+*/
 
 
-rsmq.receiveMessage({qname:config.rsmq.q2name}, function (err, resp) {
-    if (resp.id) {
-        console.log("Message received.", resp);
-        insertToDB(resp);
-
-        rsmq.deleteMessage({qname:config.rsmq.q2name, id:resp.id}, function (err, resp) {
-            if (resp===1) {
-                console.log("Message" + resp.id + "deleted." );
-            }
-            else {
-                console.log("Message not found.")
+rsmq.createQueue({qname: config.rsmq.q2name}, function (err, resp) {
+    if (!err){
+        console.log("Queue "+ resp + " created.");
+        rsmq.sendMessage({
+            qname: config.rsmq.q2name, message: JSON.stringify({
+                "symbol": "0001",
+                "side": "sell",
+                "qty": 1,
+                "order_type": "limit",
+                "price": 200,
+                "team_uid": "eETKmZ1JvPC2wM_UsA8sHw",
+                "exchange_id": 1
+            })},function(err,resp){
+            if(!err){
+                console.log("Message sent. ID:", resp);
+                rsmq.receiveMessage({qname: config.rsmq.q2name},function(err,resp){
+                    if(!err) {
+                        console.log("Message received.", resp);
+                        insertToDB(resp);
+                        rsmq.deleteMessage({qname:config.rsmq.q2name, id:resp.id},function(err,resp){
+                            if (resp === 1) {
+                                console.log("Message" + resp.id + "deleted.");
+                            }
+                            else {
+                                console.log("Message not found.")
+                            }
+                        });
+                    }
+                });
             }
         });
-
-    }
-    else {
-        console.log("No messages for me...")
     }
 });
+
+
 
 //RESTful API to let front-end query me
 app.post('/queryFromDB', function (req, res) {
