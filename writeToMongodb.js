9fd3e1563182ba775a4563a68d9e9786f10afa04
He Zhou <hezhou556@gmail.com>
2016-10-01 12:24:34 +0800

    Complete MongoDB insertion and query

--- writeToMongodb.js
+++ writeToMongodb.js
@@ -15,15 +15,35 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
 //Include redis modules
-var redis = require("redis"),
-    client = redis.createClient();
+RedisSMQ = require("rsmq");
+rsmq = new RedisSMQ( {host: "127.0.0.1", port: 6379, ns: "rsmq"} );
 
-function databaseWorker(){
-    client.on("error", function (err) {
-        console.log("Error " + err);
+/*
+ function databaseWorker(){
+ client.on("error", function (err) {
+ console.log("Error " + err);
+ });
+ }
+ */
+//databaseWorker();
+
+function insertToDB(entry){
+    mongoClient.connect("mongodb://localhost:27017/teamAce", function (err, db) {
+        if (err) {
+            console.log('an error');
+        } else {
+            console.log('no error');
+            db.createCollection('executionHistory', function (err, collection) {
+                db.collection('executionHistory').insertOne(entry, function (err, result) {
+                    console.log(result);
+                    console.log("Inserted a document into the executionHistory collection.");
+                });
+                collection.find({}).toArray(function(err, result){
+                    console.log(result);
+                });
+            });
+        }
     });
 }
 
-databaseWorker();
-
-
+insertToDB({"price":3.55});
