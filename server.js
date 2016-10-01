670d0209d5b648c92f7d7abb5ce489495c5a032f
Ace Bai <cbai@connect.ust.hk>
2016-10-01 10:45:42 +0800

    initial commit

---
+++ server.js
@@ -0,0 +1,62 @@
+'use strict';
+
+/*
+ * nodejs-express-mongoose
+ * Copyright(c) 2015 Madhusudhan Srinivasa <madhums8@gmail.com>
+ * MIT Licensed
+ */
+
+/**
+ * Module dependencies
+ */
+
+require('dotenv').config();
+
+const fs = require('fs');
+const join = require('path').join;
+const express = require('express');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const config = require('./config');
+
+const models = join(__dirname, 'app/models');
+const port = process.env.PORT || 3000;
+
+const app = express();
+const connection = connect();
+
+/**
+ * Expose
+ */
+
+module.exports = {
+  app,
+  connection
+};
+
+// Bootstrap models
+fs.readdirSync(models)
+  .filter(file => ~file.indexOf('.js'))
+  .forEach(file => require(join(models, file)));
+
+// Bootstrap routes
+require('./config/passport')(passport);
+require('./config/express')(app, passport);
+require('./config/routes')(app, passport);
+
+connection
+  .on('error', console.log)
+  .on('disconnected', connect)
+  .once('open', listen);
+
+function listen () {
+  if (app.get('env') === 'test') return;
+  app.listen(port);
+  console.log('Express app started on port ' + port);
+}
+
+function connect () {
+  var options = { server: { socketOptions: { keepAlive: 1 } } };
+  var connection = mongoose.connect(config.db, options).connection;
+  return connection;
+}
