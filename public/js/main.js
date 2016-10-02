b194e1ced1aa75c62c1b0e9d52fcec7f86262971
bliuag <bliuag@connect.ust.hk>
2016-10-02 14:43:54 +0800

    front end finished

--- public/js/main.js
+++ public/js/main.js
@@ -78,14 +78,29 @@ app.controller("BarCtrl", ["$scope","$interval","$http",function ($scope,$interv
         }).then(function successCallback(response) {
             obj=response.data;
             console.log(obj);
-            $scope.data = [ [obj["0001"].actualHold,obj["0005"].actualHold,obj["0386"].actualHold,obj["0388"].actualHold,obj["3988"].actualHold],
-                            [obj["0001"].reserved,obj["0005"].reserved,obj["0386"].reserved,obj["0388"].reserved,obj["3988"].reserved]];
-            console.log($scope.data);
+            $scope.data[0][0] = obj["0001"].actualHold;
+            $scope.data[0][1] = obj["0005"].actualHold;
+            $scope.data[0][2] = obj["0386"].actualHold;
+            $scope.data[0][3] = obj["0388"].actualHold;
+            $scope.data[0][4] = obj["3988"].actualHold;
+            $scope.data[1][0] = obj["0001"].reserved;
+            $scope.data[1][1] = obj["0005"].reserved;
+            $scope.data[1][2] = obj["0386"].reserved;
+            $scope.data[1][3] = obj["0388"].reserved;
+            $scope.data[1][4] = obj["3988"].reserved;
+
+            
+            
+            
+            
+            // [ [obj["0001"].actualHold,obj["0005"].actualHold,obj["0386"].actualHold,obj["0388"].actualHold,obj["3988"].actualHold],
+            //                 [obj["0001"].reserved,obj["0005"].reserved,obj["0386"].reserved,obj["0388"].reserved,obj["3988"].reserved]];
+            // console.log($scope.data);
         }, function errorCallback(response) {
             // called asynchronously if an error occurs
             // or server returns response with an error status.
         })
-        , 3000);
+        , 1000);
 
 }]);
 
@@ -97,19 +112,27 @@ app.controller("DoughnutCtrl", ["$scope","$interval","$http",function ($scope,$i
 //     $scope.data[0]+=100;
         
 //   },3000);
-    $scope.itv = $interval(
+    // $scope.itv = $interval(
+        setInterval(
+            function(){
         $http({
             method: 'GET',
             url: 'http://localhost:3000/getTeamInfo',
         }).then(function successCallback(response) {
             // console.log(response.data);
+            // console.log("success");
             obj=response.data;
-            $scope.data = [ obj.holdings,obj.cash ];
+            // console.log( obj.holdings,obj.cash);
+            $scope.data[0]=obj.holdings;
+            $scope.data[1]=obj.cash;
+            //  = [ obj.holdings,obj.cash ];
         }, function errorCallback(response) {
+            // console.log("fail");
             // called asynchronously if an error occurs
             // or server returns response with an error status.
-        })
-        , 3000);
+        })}
+        , 1000);
+        // , 3000);
 }]);
 
 
